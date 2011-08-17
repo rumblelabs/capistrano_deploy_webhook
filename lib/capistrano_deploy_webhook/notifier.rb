2df57e853db55a772f66f5cde1b6635c3e3fedd0
Phil McClure <pmcclure@rumblelabs.com>
2011-08-17 10:45:04 +0100

    First commit

---
+++ lib/capistrano_deploy_webhook/notifier.rb
@@ -0,0 +1,29 @@
+require 'net/http'
+require 'uri'
+
+unless Capistrano::Configuration.respond_to?(:instance)
+  abort "rails_deploy_notification requires Capistrano 2"
+end
+
+Capistrano::Configuration.instance.load do
+  after :deploy, "notify:post_request"
+
+  namespace :notify do
+    task :post_request do
+      puts "*** Notification POST to #{self[:notify_url]}"
+      url = URI.parse("#{self[:notify_url]}")
+      req = Net::HTTP::Post.new(url.path)
+      req.set_form_data(
+        {'app_name' => self[:application], 
+         'app' => self[:application], 
+         'user' => self[:user], 
+         #'sha' => self[:current_revision], 
+         #'prev_sha' => self[:previous_revision], 
+         'url' => self[:url]}, 
+         ';')
+      res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
+    end
+  end
+end
+
+
