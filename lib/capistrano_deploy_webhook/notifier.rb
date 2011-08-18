e267fef64334e817db48c6d7a7a9cd7b71ff9ec5
Phil McClure <pmcclure@rumblelabs.com>
2011-08-18 09:20:51 +0100

    Removed some redundant code

--- lib/capistrano_deploy_webhook/notifier.rb
+++ lib/capistrano_deploy_webhook/notifier.rb
@@ -11,12 +11,11 @@ Capistrano::Configuration.instance.load do
   namespace :notify do
     task :post_request do
       application_name = `pwd`.chomp.split('/').last
-      puts "*** Notification POST to #{self[:notify_url]}"
+      puts "*** Notification POST to #{self[:notify_url]} for #{application_name}"
       url = URI.parse("#{self[:notify_url]}")
       req = Net::HTTP::Post.new(url.path)
       req.set_form_data(
-        {'app_name' => application_name, 
-         'app' => application_name, 
+        {'app' => application_name, 
          'user' => self[:user], 
          'sha' => self[:current_revision], 
          'prev_sha' => self[:previous_revision], 
