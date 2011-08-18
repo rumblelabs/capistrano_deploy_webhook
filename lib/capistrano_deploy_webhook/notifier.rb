6bf085a57d0929550cfb6aa10c6aea1dbb3568c9
Phil McClure <pmcclure@rumblelabs.com>
2011-08-18 11:21:54 +0100

    Moved git related data out of capistrano scope to make sure getting local git data

--- lib/capistrano_deploy_webhook/notifier.rb
+++ lib/capistrano_deploy_webhook/notifier.rb
@@ -5,24 +5,25 @@ unless Capistrano::Configuration.respond_to?(:instance)
   abort "capistrano_deploy_webhook requires Capistrano 2"
 end
 
+GIT_USER_EMAIL = `git config --get user.email`
+GIT_CURRENT_REV = `git rev-parse HEAD`
+GIT_CURRENT_REV_SHORT = git_current_rev[0,7]
+
 Capistrano::Configuration.instance.load do
   after :deploy, "notify:post_request"
 
   namespace :notify do
     task :post_request do
       application_name = `pwd`.chomp.split('/').last
-      git_user_email = `git config --get user.email`
-      git_current_rev = `git rev-parse HEAD`
-      git_current_rev_short = git_current_rev[0,7]
 
       puts "*** Notification POST to #{self[:notify_url]} for #{application_name}"
       url = URI.parse("#{self[:notify_url]}")
       req = Net::HTTP::Post.new(url.path)
       req.set_form_data(
         {'app' => application_name, 
-         'user' => git_user_email, 
-         'head' => git_current_rev_short, 
-         'head_long' => git_current_rev,
+         'user' => GIT_USER_EMAIL, 
+         'head' => GIT_CURRENT_REV_SHORT, 
+         'head_long' => GIT_CURRENT_REV,
          'prev_head' => self[:previous_revision], 
          'url' => self[:url]}, 
          ';')
